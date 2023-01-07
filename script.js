c45fbea4a3a3bececc74df4af6884918be5bd341
shravany856 <115397306+shravany856@users.noreply.github.com>
2023-01-08 01:11:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+data = [
+    {id:"bitcoin",symbol:"btc",name:"Bitcoin",image:"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",current_price:16934.1,market_cap:326016762628,market_cap_rank:1,fully_diluted_valuation:355562887209,total_volume:17526781094,high_24h:17021.29,low_24h:16710.14,price_change_24h:149.51,price_change_percentage_24h:0.89075,market_cap_change_24h:2823408974,market_cap_change_percentage_24h:0.8736,circulating_supply:19254968.0,total_supply:21000000.0,max_supply:21000000.0,ath:69045,ath_change_percentage:-75.47298,ath_date:"2021-11-10T14:24:11.849Z",atl:67.81,atl_change_percentage:24874.0037,atl_date:"2013-07-06T00:00:00.000Z",roi:null,last_updated:"2023-01-07T10:04:57.152Z"},
+    {id:"ethereum",symbol:"eth",name:"Ethereum",image:"https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",current_price:1265.61,market_cap:152481527289,market_cap_rank:2,fully_diluted_valuation:152481527289,total_volume:4756335756,high_24h:1275.78,low_24h:1242.1,price_change_24h:17.75,price_change_percentage_24h:1.42228,market_cap_change_24h:2053337609,market_cap_change_percentage_24h:1.365,circulating_supply:120524497.701488,total_supply:120524497.701488,max_supply:null,ath:4878.26,ath_change_percentage:-74.05118,ath_date:"2021-11-10T14:24:19.604Z",atl:0.432979,atl_change_percentage:292258.64606,atl_date:"2015-10-20T00:00:00.000Z",roi:{times:98.92116491501625,currency:"btc",percentage:9892.116491501625},last_updated:"2023-01-07T10:05:22.620Z"},
+    {id:"tether",symbol:"usdt",name:"Tether",image:"https://assets.coingecko.com/coins/images/325/large/Tether.png?1668148663",current_price:1.0,market_cap:66290801966,market_cap_rank:3,fully_diluted_valuation:66290801966,total_volume:22921766095,high_24h:1.002,low_24h:0.997607,price_change_24h:0.00032713,price_change_percentage_24h:0.03271,market_cap_change_24h:15610271,market_cap_change_percentage_24h:0.02355,circulating_supply:66272490385.2966,total_supply:66272490385.2966,max_supply:null,ath:1.32,ath_change_percentage:-24.41662,ath_date:"2018-07-24T00:00:00.000Z",atl:0.572521,atl_change_percentage:74.6731,atl_date:"2015-03-02T00:00:00.000Z",roi:null,last_updated:"2023-01-07T10:00:17.290Z"},
+    {id:"usd-coin",symbol:"usdc",name:"USD Coin",image:"https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png?1547042389",current_price:1.001,market_cap:43761737658,market_cap_rank:4,fully_diluted_valuation:43754416236,total_volume:2150603730,high_24h:1.002,low_24h:0.997155,price_change_24h:0.00056854,price_change_percentage_24h:0.05685,market_cap_change_24h:-50260484.03969574,market_cap_change_percentage_24h:-0.11472,circulating_supply:43739597961.2957,total_supply:43747052943.0857,max_supply:null,ath:1.17,ath_change_percentage:-14.70301,ath_date:"2019-05-08T00:40:28.300Z",atl:0.891848,atl_change_percentage:12.15848,atl_date:"2021-05-19T13:14:05.611Z",roi:null,last_updated:"2023-01-07T10:05:02.751Z"},
+    {id:"binancecoin",symbol:"bnb",name:"BNB",image:"https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850",current_price:261.3,market_cap:42664618916,market_cap_rank:5,fully_diluted_valuation:52260423140,total_volume:412828238,high_24h:262.55,low_24h:255.42,price_change_24h:4.81,price_change_percentage_24h:1.8761,market_cap_change_24h:780146561,market_cap_change_percentage_24h:1.86262,circulating_supply:163276974.63,total_supply:163276974.63,max_supply:200000000.0,ath:686.31,ath_change_percentage:-61.9279,ath_date:"2021-05-10T07:24:17.097Z",atl:0.0398177,atl_change_percentage:656119.14403,atl_date:"2017-10-19T00:00:00.000Z",roi:null,last_updated:"2023-01-07T10:04:55.515Z"},
+    {id:"ripple",symbol:"xrp",name:"XRP",image:"https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731",current_price:0.343448,market_cap:17367073986,market_cap_rank:6,fully_diluted_valuation:34346874713,total_volume:583592367,high_24h:0.346053,low_24h:0.334424,price_change_24h:0.00768554,price_change_percentage_24h:2.28898,market_cap_change_24h:381064852,market_cap_change_percentage_24h:2.2434,circulating_supply:50563767827.0,total_supply:99989171756.0,max_supply:100000000000.0,ath:3.4,ath_change_percentage:-89.8911,ath_date:"2018-01-07T00:00:00.000Z",atl:0.00268621,atl_change_percentage:12689.26307,atl_date:"2014-05-22T00:00:00.000Z",roi:null,last_updated:"2023-01-07T10:05:17.441Z"},
+    {id:"binance-usd",symbol:"busd",name:"Binance USD",image:"https://assets.coingecko.com/coins/images/9576/large/BUSD.png?1568947766",current_price:1.001,market_cap:16434367031,market_cap_rank:7,fully_diluted_valuation:16434367031,total_volume:5404658293,high_24h:1.004,low_24h:0.999316,price_change_24h:0.0002719,price_change_percentage_24h:0.02718,market_cap_change_24h:-250497422.83473396,market_cap_change_percentage_24h:-1.50135,circulating_supply:16424967238.39,total_supply:16424967238.39,max_supply:null,ath:1.15,ath_change_percentage:-13.33705,ath_date:"2020-03-13T02:35:42.953Z",atl:0.901127,atl_change_percentage:11.00178,atl_date:"2021-05-19T13:04:37.445Z",roi:null,last_updated:"2023-01-07T10:05:23.949Z"},
+    {id:"dogecoin",symbol:"doge",name:"Dogecoin",image:"https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1547792256",current_price:0.072537,market_cap:9989600456,market_cap_rank:8,fully_diluted_valuation:null,total_volume:366684795,high_24h:0.072939,low_24h:0.070246,price_change_24h:0.00157491,price_change_percentage_24h:2.21937,market_cap_change_24h:231731617,market_cap_change_percentage_24h:2.37482,circulating_supply:137623026383.705,total_supply:null,max_supply:null,ath:0.731578,ath_change_percentage:-90.07168,ath_date:"2021-05-08T05:08:23.458Z",atl:8.69e-05,atl_change_percentage:83479.10851,atl_date:"2015-05-06T00:00:00.000Z",roi:null,last_updated:"2023-01-07T10:05:22.290Z"},
+    {id:"cardano",symbol:"ada",name:"Cardano",image:"https://assets.coingecko.com/coins/images/975/large/cardano.png?1547034860",current_price:0.275011,market_cap:9641001651,market_cap_rank:9,fully_diluted_valuation:12379649491,total_volume:271761396,high_24h:0.279904,low_24h:0.268176,price_change_24h:0.0028212,price_change_percentage_24h:1.03648,market_cap_change_24h:92157664,market_cap_change_percentage_24h:0.96512,circulating_supply:35045020830.3234,total_supply:45000000000.0,max_supply:45000000000.0,ath:3.09,ath_change_percentage:-91.08563,ath_date:"2021-09-02T06:00:10.474Z",atl:0.01925275,atl_change_percentage:1329.29574,atl_date:"2020-03-13T02:22:55.044Z",roi:null,last_updated:"2023-01-07T10:05:26.902Z"},
+    {id:"matic-network",symbol:"matic",name:"Polygon",image:"https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912",current_price:0.803592,market_cap:7206527450,market_cap_rank:10,fully_diluted_valuation:8038093037,total_volume:217480261,high_24h:0.811581,low_24h:0.774855,price_change_24h:0.02588947,price_change_percentage_24h:3.32897,market_cap_change_24h:232159770,market_cap_change_percentage_24h:3.32876,circulating_supply:8965469069.28493,total_supply:10000000000.0,max_supply:10000000000.0,ath:2.92,ath_change_percentage:-72.42539,ath_date:"2021-12-27T02:08:34.307Z",atl:0.00314376,atl_change_percentage:25479.18212,atl_date:"2019-05-10T00:00:00.000Z",roi:{times:304.54820101343256,currency:"usd",percentage:30454.82010134326},last_updated:"2023-01-07T10:05:03.377Z"}
+    ]
+
+const tableElement = document.getElementById("table");
+// const renderdata =((item) => {
+   
+// })
+
+// const fetchData =() =>{
+//     const apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+//     const promise = fetch(apiUrl);
+
+//     promise.then(res => res.json())
+//     .then((item => {
+//         console.log("list",item);
+
+//         for(let i=0; item.length; i++){
+//             renderdata(item[i]);
+//         }
+//     }))
+
+// }
+// fetchData();
+   
+
+
+
+
+  function empdata(){
+    data.map((item) =>{
+        let Item = document.createElement("tr");
+        console.log("abc",Item);
+        Item.innerHTML = `
+        <td style= "padding : 40px"> ${item.id}</td> 
+        <td> <img src="${item.image}" height="40" width="40"> ${item.name}</td>
+        <td> ${item.symbol.toUpperCase()}  </td>
+        <td> $${item.current_price}  </td>
+        <td> $${item.total_volume}  </td>
+        <td style="color: green"> ${item.percentage} </td>
+        <td> Mkt Cap : $${item.market_cap}</td>
+        
+        `
+        Item.style.borderBottom= "solid 5px";
+        Item.style.borderColor= "white";
+        Item.style.borderWidth= " 5px";
+
+        tableElement.append(Item);
+       
+       })
+      
+    }
+  
+  empdata();
+
+    
